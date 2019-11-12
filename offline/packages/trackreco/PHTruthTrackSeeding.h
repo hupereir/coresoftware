8b3538b113a09f874b382330ff3b1886b664b7a5
Hugo Pereira Da Costa <hugo.pereira-da-costa@cea.fr>
2019-11-12 11:04:21 -0500

    added some overrride for reimplemented virtual methods added propoer initialization of all members removed unnecessary destructor.

--- offline/packages/trackreco/PHTruthTrackSeeding.h
+++ offline/packages/trackreco/PHTruthTrackSeeding.h
@@ -30,23 +30,17 @@ class TrkrHitTruthAssoc;
 class PHTruthTrackSeeding : public PHTrackSeeding
 {
  public:
+
   PHTruthTrackSeeding(const std::string& name = "PHTruthTrackSeeding");
-  virtual ~PHTruthTrackSeeding() {}
 
   unsigned int get_min_clusters_per_track() const
-  {
-    return _min_clusters_per_track;
-  }
+  { return _min_clusters_per_track; }
 
   void set_min_clusters_per_track(unsigned int minClustersPerTrack)
-  {
-    _min_clusters_per_track = minClustersPerTrack;
-  }
+  { _min_clusters_per_track = minClustersPerTrack; }
 
   const std::set<unsigned int>& get_seeding_layers() const
-  {
-    return _seeding_layers;
-  }
+  { return _seeding_layers; }
 
   void set_seeding_layers(const unsigned int a[], const unsigned int n)
   {
@@ -55,34 +49,35 @@ class PHTruthTrackSeeding : public PHTrackSeeding
   }
 
  protected:
-  int Setup(PHCompositeNode* topNode);
+  int Setup(PHCompositeNode* topNode) override;
 
-  int Process(PHCompositeNode* topNode);
+  int Process(PHCompositeNode* topNode) override;
 
-  int End();
+  int End() override;
 
  private:
   /// fetch node pointers
   int GetNodes(PHCompositeNode* topNode);
 
-  PHG4TruthInfoContainer* _g4truth_container;
+  PHG4TruthInfoContainer* _g4truth_container = nullptr;
 
-  PHG4HitContainer* phg4hits_tpc;
-  PHG4HitContainer* phg4hits_intt;
-  PHG4HitContainer* phg4hits_mvtx;
+  PHG4HitContainer* phg4hits_tpc = nullptr;
+  PHG4HitContainer* phg4hits_intt = nullptr;
+  PHG4HitContainer* phg4hits_mvtx = nullptr;
 
-  TrkrHitTruthAssoc* hittruthassoc;
-  TrkrClusterHitAssoc* clusterhitassoc;
+  TrkrHitTruthAssoc* hittruthassoc = nullptr;
+  TrkrClusterHitAssoc* clusterhitassoc = nullptr;
 
   //SvtxHitMap* hitsmap;
-  //PHG4CellContainer* cells_svtx;
-  //PHG4CellContainer* cells_intt;
-  //PHG4CellContainer* cells_maps;
+  //PHG4CellContainer* cells_svtx = nullptr;
+  //PHG4CellContainer* cells_intt = nullptr;
+  //PHG4CellContainer* cells_maps = nullptr;
 
   /// seeding layers
   std::set<unsigned int> _seeding_layers;
 
-  unsigned int _min_clusters_per_track;
+  unsigned int _min_clusters_per_track = 0;
+
 };
 
 #endif
